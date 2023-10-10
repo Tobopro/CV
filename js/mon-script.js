19f1722d499a8df42a20694be5c1daf21b727dbf
Tobopro <145339180+Tobopro@users.noreply.github.com>
2023-10-10 16:03:17 +0200

    Add files via upload

---
+++ js/mon-script.js
@@ -0,0 +1,16 @@
+// mon-script.js
+
+// Attend que le document soit chargé
+document.addEventListener("DOMContentLoaded", function() {
+    // Active le dropdown
+    $('.dropdown-toggle').dropdown();
+})
+// $(document).ready(function() {
+//     // Écoutez le clic sur tous les boutons d'accordéon
+//     $('.accordion-button').click(function() {
+//         // Fermez tous les éléments d'accordéon sauf celui qui a été cliqué
+//         $('.accordion-button').not(this).addClass('collapsed');
+//         $('.accordion-collapse').not($(this).attr('data-bs-target')).removeClass('show');
+//     });
+// });
+;
